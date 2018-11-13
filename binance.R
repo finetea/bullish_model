cc3513aba05c06354296f8898e4b4da28b4cda3b
kim sung kyu <finetea@gmail.com>
2018-11-13 15:29:34 +0900

    learning RF model

--- binance.R
+++ binance.R
@@ -13,6 +13,24 @@ suppressPackageStartupMessages({
   library(rstudioapi)
   library(randomForest)
   library(xgboost)      # Extreme Gradient Boosting
+  
+  library(foreach)
+  library(randomForest)
+  library(rpart)
+  library(rpart.plot)
+  library(ggplot2)
+  library(plyr)
+  library(corrgram)
+  library(corrplot)
+  library(kernlab)
+  library(deepnet)
+  library(lattice)
+  library(caret)
+  library(caTools)
+  library(e1071)
+  library(stringr)
+  library(magrittr)  
+  library(telegram.bot)
 })
 
 ### set path
@@ -268,9 +286,9 @@ for (cp in checkpoints) {
   
 }
 
-
-
 train <- do.call(rbind,train)
+
+
 
 
 #set column names
@@ -307,4 +325,316 @@ print (paste(time.taken.total, "for total process"))
 
 ##########################################################################################
 # create labels
-nrow(trades)
+
+label_rates = c(0.3, 0.4, 0.5)  #fee of binance is 0.1 for each trade
+label_cond = 1.0+label_rates/100 #percent
+label_far = mns(5)
+label_far_margin = label_far+mns(5)
+
+i <- 1
+labels = list()
+
+for (cp in checkpoints) {
+  checkpoint = as.POSIXct(cp, origin="1970-01-01", tz="UTC")
+  
+  #make smaller chunk of the 'trades' in every day with the size of day + 1hour(MAX_SPAN). 
+  #because the 'trades' object is so huge that the subset takes too much time.
+  size_smaller = hrs(24)
+  if ((cp - as.numeric(checkpoints[1])) %% size_smaller == 0) {
+    #print ("make trades_smaller")
+    trades_smaller <- trades[trades$date >= (cp-MAX_SPAN-MAX_SPAN) & trades$date < (cp+size_smaller),]
+    #print (trades_smaller[1])
+    #print (trades_smaller[NROW(trades_smaller)])
+  }
+  
+  #print(checkpoint)
+  cp_price_candidates <- trades_smaller[trades_smaller$date>=(cp-mns(1)) & trades_smaller$date<=(cp),]
+  cp_price <- if(NROW(cp_price_candidates)>0) cp_price_candidates$price[NROW(cp_price_candidates)] else 0
+  label_price_candidates <- trades_smaller[trades_smaller$date>=(cp+label_far) & trades_smaller$date<=(cp+label_far_margin),]
+  label_price <- if(NROW(label_price_candidates)>0) label_price_candidates$price[1] else 0
+  label <- if(cp_price>0 & label_price>0) (label_price - (label_cond*cp_price)) else rep(0,NROW(label_rates)) #label_price is higher than the checkpoint price
+  label <- label>0
+  
+  labels[i] <- list(c(checkpoint, label))
+  i <- i+1
+}
+labels <- do.call(rbind,labels)
+colnames(labels) <- c("checkpoint", str_replace(paste("label_",label_rates,sep = ''), '\\.', '_'))
+
+apply(labels[,c(2:4)], 2, sum)/NROW(labels)
+
+
+
+
+
+
+##########################################################################################
+# combine data
+
+#train <- train[c(1:NROW(checkpoints)),]
+NROW(train)
+NROW(labels)
+
+colnames(labels)
+
+train_data = cbind(train, labels[,2:4])
+train_data<-as.data.table(train_data)
+typeof(train_data)
+colnames(train_data)
+
+train_data <- train_data %>% select(-checkpoint, -label_0_4, -label_0_5)
+colnames(train_data)
+
+table(train_data$label_0_3)
+typeof(train_data$label_0_3)
+
+train_idx = sample.split(train_data$label_0_3, SplitRatio = .75)
+train.set = train_data[ train_idx,]
+test.set  = train_data[!train_idx,]
+nrow(train.set)/nrow(train_data)
+nrow(test.set)/nrow(train_data)
+
+sum(train_data$label_0_3)/NROW(train_data)
+sum(train.set$label_0_3)/NROW(train.set)
+sum(test.set$label_0_3)/NROW(test.set)
+
+train.set$label_0_3 <- as.factor(train.set$label_0_3)
+test.set$label_0_3 <- as.factor(test.set$label_0_3)
+typeof(train_data$label_0_3)
+typeof(train.set$label_0_3)
+typeof(test.set$label_0_3)
+
+
+
+##########################################################################################
+#learn by random forest
+
+start.time.learning <- Sys.time()
+
+set.seed(777)
+modelRF1 <- randomForest(label_0_3 ~.
+                         , data=train.set, importance=TRUE, ntree=250, na.action=na.roughfix)
+predRF1 = predict(modelRF1, test.set)
+test.set$predicted <- predRF1
+
+subset(test.set, select=c(label_0_3, predicted))
+
+table(test.set$predicted, test.set$label_0_3)
+confusionMatrix(test.set$predicted, test.set$label_0_3)
+
+
+end.time.learning <- Sys.time()
+time.taken.learning <- end.time.learning - start.time.learning
+print (paste(time.taken.learning, "for learning"))
+
+
+
+##########################
+#with adjusted train_data 
+train_data = cbind(train, labels[,2:4])
+train_data<-as.data.table(train_data)
+typeof(train_data)
+colnames(train_data)
+
+train_data <- train_data %>% select(-checkpoint, -label_0_4, -label_0_5)
+colnames(train_data)
+
+
+idx <- (train_data$label_0_3==0)
+t_0 <- train_data[idx,]
+t_1 <- train_data[!idx,]
+NROW(t_0)
+
+#resize
+N <- NROW(t_0)
+idx<-sort(sample(1:N, N/5, replace = FALSE))
+t_0 <- t_0[idx]
+train_data <- rbind(t_0, t_1)
+NROW(train_data)
+table(train_data$label_0_3)
+
+train_idx = sample.split(train_data$label_0_3, SplitRatio = .75)
+train.set = train_data[ train_idx,]
+test.set  = train_data[!train_idx,]
+nrow(train.set)/nrow(train_data)
+nrow(test.set)/nrow(train_data)
+
+sum(train_data$label_0_3)/NROW(train_data)
+sum(train.set$label_0_3)/NROW(train.set)
+sum(test.set$label_0_3)/NROW(test.set)
+
+train.set$label_0_3 <- as.factor(train.set$label_0_3)
+test.set$label_0_3 <- as.factor(test.set$label_0_3)
+typeof(train_data$label_0_3)
+typeof(train.set$label_0_3)
+typeof(test.set$label_0_3)
+
+
+
+start.time.learning <- Sys.time()
+
+set.seed(777)
+modelRF1 <- randomForest(label_0_3 ~.
+                         , data=train.set, importance=TRUE, ntree=250, na.action=na.roughfix)
+predRF1 = predict(modelRF1, test.set)
+test.set$predicted <- predRF1
+
+subset(test.set, select=c(label_0_3, predicted))
+
+table(test.set$predicted, test.set$label_0_3)
+confusionMatrix(test.set$predicted, test.set$label_0_3)
+
+
+end.time.learning <- Sys.time()
+time.taken.learning <- end.time.learning - start.time.learning
+print (paste(time.taken.learning, "for learning"))
+
+
+
+
+
+##########################
+#with adjusted train_data and another label
+train_data = cbind(train, labels[,2:4])
+train_data<-as.data.table(train_data)
+typeof(train_data)
+colnames(train_data)
+
+train_data <- train_data %>% select(-checkpoint, -label_0_3, -label_0_5) #using 0.4 this time.
+colnames(train_data)
+
+
+idx <- (train_data$label_0_4==0)
+t_0 <- train_data[idx,]
+t_1 <- train_data[!idx,]
+NROW(t_0)
+
+#resize
+N <- NROW(t_0)
+idx<-sort(sample(1:N, N/5, replace = FALSE))
+t_0 <- t_0[idx]
+train_data <- rbind(t_0, t_1)
+NROW(train_data)
+table(train_data$label_0_4)
+
+train_idx = sample.split(train_data$label_0_4, SplitRatio = .75)
+train.set = train_data[ train_idx,]
+test.set  = train_data[!train_idx,]
+nrow(train.set)/nrow(train_data)
+nrow(test.set)/nrow(train_data)
+
+sum(train_data$label_0_4)/NROW(train_data)
+sum(train.set$label_0_4)/NROW(train.set)
+sum(test.set$label_0_4)/NROW(test.set)
+
+train.set$label_0_4 <- as.factor(train.set$label_0_4)
+test.set$label_0_4 <- as.factor(test.set$label_0_4)
+typeof(train_data$label_0_4)
+typeof(train.set$label_0_4)
+typeof(test.set$label_0_4)
+
+
+
+start.time.learning <- Sys.time()
+
+set.seed(777)
+modelRF1 <- randomForest(label_0_4 ~.
+                         , data=train.set, importance=TRUE, ntree=250, na.action=na.roughfix)
+predRF1 = predict(modelRF1, test.set)
+test.set$predicted <- predRF1
+
+subset(test.set, select=c(label_0_4, predicted))
+
+table(test.set$predicted, test.set$label_0_4)
+confusionMatrix(test.set$predicted, test.set$label_0_4)
+
+
+end.time.learning <- Sys.time()
+time.taken.learning <- end.time.learning - start.time.learning
+print (paste(time.taken.learning, "for learning"))
+
+
+
+
+
+
+
+
+##########################
+#with adjusted train_data and another label
+train_data = cbind(train, labels[,2:4])
+train_data<-as.data.table(train_data)
+typeof(train_data)
+colnames(train_data)
+
+train_data <- train_data %>% select(-checkpoint, -label_0_3, -label_0_4) #using 0.5 this time.
+colnames(train_data)
+
+
+idx <- (train_data$label_0_5==0)
+t_0 <- train_data[idx,]
+t_1 <- train_data[!idx,]
+NROW(t_0)
+
+#resize
+N <- NROW(t_0)
+idx<-sort(sample(1:N, N/5, replace = FALSE))
+t_0 <- t_0[idx]
+train_data <- rbind(t_0, t_1)
+NROW(train_data)
+table(train_data$label_0_5)
+
+train_data$label <- train_data$label_0_5
+train_data <- train_data %>% select(-label_0_5)
+colnames(train_data)
+
+
+train_idx = sample.split(train_data$label, SplitRatio = .75)
+train.set = train_data[ train_idx,]
+test.set  = train_data[!train_idx,]
+nrow(train.set)/nrow(train_data)
+nrow(test.set)/nrow(train_data)
+
+sum(train_data$label)/NROW(train_data)
+sum(train.set$label)/NROW(train.set)
+sum(test.set$label)/NROW(test.set)
+
+train.set$label <- as.factor(train.set$label)
+test.set$label <- as.factor(test.set$label)
+typeof(train_data$label)
+typeof(train.set$label)
+typeof(test.set$label)
+
+colnames(train.set)
+
+start.time.learning <- Sys.time()
+
+set.seed(777)
+modelRF1 <- randomForest(label ~.
+                         , data=train.set, importance=TRUE, ntree=250, na.action=na.roughfix)
+predRF1 = predict(modelRF1, test.set)
+test.set$predicted <- predRF1
+
+subset(test.set, select=c(label, predicted))
+
+table(test.set$predicted, test.set$label)
+result <- confusionMatrix(test.set$predicted, test.set$label)
+result <- paste(result, collapse = "\n")
+
+
+end.time.learning <- Sys.time()
+time.taken.learning <- end.time.learning - start.time.learning
+print (paste(time.taken.learning, "for learning"))
+
+
+#reporting
+bot <- Bot(token = "440149496:AAGsZfEN0ZG1GENfeIpaFW-FvPjIJIPkZ2g")
+chat_id <- "356316439"
+
+# Send message
+#bot$sendMessage(chat_id = chat_id, text = "*model is ready*", parse_mode = "Markdown")
+bot$sendMessage(chat_id = chat_id, text = as.character(start.time.learning))
+bot$sendMessage(chat_id = chat_id, text = as.character(end.time.learning))
+bot$sendMessage(chat_id = chat_id, text = result)
+
+
